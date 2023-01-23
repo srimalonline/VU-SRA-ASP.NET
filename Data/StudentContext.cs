3dd76543caa3f5f5c52e9cdd53020a8235c17e8c
srimalonline <dilankasrimal23@gmail.com>
2023-01-23 22:34:35 +0530

    this is a dev branch

--- Data/StudentContext.cs
+++ Data/StudentContext.cs
@@ -1,7 +1,14 @@
+using VU_SRA_ASP.NET.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace VU_SRA_ASP.NET.Data
 {
-    public class StudentContext 
+    public class StudentContext : DbContext
     {
-        
+        public StudentContext(DbContextOptions options) :base(options)
+        {
+
+        }
+        public Data 
     }
 }
--- Models/Student.cs
+++ Models/Student.cs
@@ -1,12 +1,16 @@
+using System;
+using System.Collections.Generic;
+
 namespace VU_SRA_ASP.NET.Models
 {
+    
     public class Student
     {
-        public int Id {get; set;}
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
 
-        public string? name {get; set;}
-
-        public string? email {get; set;}
-        public string? password {get; set;}
+        public ICollection<Enrollment> Enrollments { get; set; }
     }
 }
